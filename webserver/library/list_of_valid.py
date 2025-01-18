79a410312319e5d516419cd40f9c50b0e465f760
Ben Payne <ben.is.located@gmail.com>
2025-01-18 07:34:41 -0500

    added kilometer; added abbreviations to enable search

--- webserver/library/list_of_valid.py
+++ webserver/library/list_of_valid.py
@@ -15,12 +15,12 @@ node_types = [
 
 
 dimension_mass_units = [
-    "kilogram",
-    "gram",
+    "kilogram (kg)",
+    "gram (g)",
     "stone",  # https://en.wikipedia.org/wiki/Stone_(unit)
     "ton",  # https://en.wikipedia.org/wiki/Long_ton
-    "ounce",  # https://en.wikipedia.org/wiki/Ounce
-    "pound",
+    "ounce (oz)",  # https://en.wikipedia.org/wiki/Ounce
+    "pound (lb)",
 ]  # https://en.wikipedia.org/wiki/Pound_(mass)
 
 dimension_time_units = [
@@ -31,29 +31,30 @@ dimension_time_units = [
     "day",
     "hour",
     "minute",
-    "second",
-    "millisecond",
-    "microsecond",
-    "picosecond",
-    "femptosecond",
+    "second (s)",
+    "millisecond (ms)",
+    "microsecond (us)",
+    "picosecond (ps)",
+    "femptosecond (fs)",
 ]
 
 # https://en.wikipedia.org/wiki/Length
 dimension_length_units = [
-    "meter",
-    "centimeter",
-    "millimeter",
+    "kilometer (km)",
+    "meter (m)",
+    "centimeter (cm)",
+    "millimeter (mm)",
     "feet",
     "foot",  # https://en.wikipedia.org/wiki/Foot_(unit)
     "furlong",  # https://en.wikipedia.org/wiki/Furlong
-    "mile",  # https://en.wikipedia.org/wiki/Mile
+    "mile (mi)",  # https://en.wikipedia.org/wiki/Mile
     "league",  # https://en.wikipedia.org/wiki/League_(unit)
     "fathom",  # https://en.wikipedia.org/wiki/Fathom
     "nautical mile",  # https://en.wikipedia.org/wiki/Nautical_mile
     "yard",  # https://en.wikipedia.org/wiki/Yard
 ]
 
-dimension_temperature_units = ["fahrenheit", "celsius", "kelvin"]
+dimension_temperature_units = ["fahrenheit (F)", "celsius (C)", "kelvin (K)"]
 dimension_electric_charge_units = ["coulomb"]  # https://en.wikipedia.org/wiki/Coulomb
 dimension_amount_of_substance_units = ["mol"]
 dimension_luminous_intensity_units = ["candela"]
