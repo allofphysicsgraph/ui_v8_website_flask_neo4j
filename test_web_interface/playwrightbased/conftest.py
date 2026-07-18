4f7288d96d744fc4125bd409a7cd2a18d5aba6a0
Ben Payne <ben.is.located@gmail.com>
2026-07-18 15:35:33 -0400

    ignore self-signed cert errors

---
+++ test_web_interface/playwrightbased/conftest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Ben Payne
+# Physics Derivation Graph
+# https://allofphysics.com
+# Creative Commons Attribution 4.0 International License
+# https://creativecommons.org/licenses/by/4.0/
+
+
+# configure your browser context to ignore HTTPS errors.
+# this file is found by `pytest-playwright`
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {**browser_context_args, "ignore_https_errors": True}
+
+
+#EOF
